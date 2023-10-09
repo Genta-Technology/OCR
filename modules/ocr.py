02bcb9e7e4e5ba77e74559a81d0de4dafeb57368
Alifais Farrel Ramdhani <farrellabindonesia@gmail.com>
2023-10-08 19:10:22 -0700

    New template
    For example and setup purposes

---
+++ modules/ocr.py
@@ -0,0 +1,36 @@
+# This is the list of function that will be used for the OCR program
+
+# List of function needed:
+# 1. PDF 2 Markdown
+# 2. Image 2 Markdwon
+# 3. Latex to PDF
+# 4. Markdown to PDF
+
+#Example template
+
+def print_something(text_print):
+    """
+    This is the purpose of the function
+    ex: Print the inputted text and return true
+    
+    :param text_print: Input String
+    :type text_print: str
+    :return: True
+    :rtype: boolean
+
+    """
+
+    print(text_print)
+
+    return True
+
+def print_something_test(text_print):
+    """
+    This is the purpose of the function
+    ex: Print the inputted text
+
+    :param text_print: Input String
+    :type text_print: str
+    """
+
+    print(text_print)
---
+++ modules/playground.py
@@ -0,0 +1,7 @@
+# This is mainly for testing purpose and will be deleted upon merging into main
+
+# Import the function
+from ocr import print_something_test
+
+# Run the function and add other code:
+print_something_test("Hello World")
